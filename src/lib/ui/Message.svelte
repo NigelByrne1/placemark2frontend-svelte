a981fba32f95b6bd05877d9b53182bb440184719
Nigel <nigelbyrnedesign@gmail.com>
2025-05-23 23:45:53 +0100

    shell-login-signup-home

---
+++ src/lib/ui/Message.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let { message } = $props();
+</script>
+
+<article class="message has-background-black has-text-white">
+  <div class="message-body">
+    {message}
+  </div>
+</article>
---
+++ src/routes/login/LoginForm.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import Message from "$lib/ui/Message.svelte";
+  import UserCredentials from "$lib/ui/UserCredentials.svelte";
+
+  let email = $state("");
+  let password = $state("");
+  let message = $state("");
+
+  async function login() {
+    const success = false;
+    if (success) {
+      goto("/placemark");
+    } else {
+      message = "Error Trying to login in";
+    }
+  }
+</script>
+
+<div class="box has-background-danger">
+  {#if message}
+    <Message {message} />
+  {/if}
+  <UserCredentials bind:email bind:password />
+  <button onclick={() => login()} class="button">Log In</button>.
+  <p class="has-text-centered has-text-white">
+    or <a href="/" data-cy="home-redirect"> Head back to the homepage </a>
+  </p>
+</div>
